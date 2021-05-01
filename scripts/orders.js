b82eeb55dc9f47f1919f33392003122c0efc6653
Marcos Adriano <marcosadriano740@gmail.com>
2021-05-01 16:45:12 -0300

    fix: button on shipping

--- scripts/orders.js
+++ scripts/orders.js
@@ -1,9 +1,10 @@
 const buttonOrder = document.querySelectorAll('[data-open-order]')
 const orders = document.querySelectorAll('[data-order]')
-console.log(orders[0].dataset.order)
+
 
 buttonOrder.forEach((button, index) => {
   button.addEventListener('click', e => {
     orders[index].classList.toggle('open')
+    console.log(orders[index].dataset.order)
   })
 })
