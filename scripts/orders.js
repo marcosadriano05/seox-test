37b3115669c197ebf79bbe0e520f0202072cdae9
Marcos Adriano <marcosadriano740@gmail.com>
2021-05-01 17:38:48 -0300

    feat: animation in button details

--- scripts/orders.js
+++ scripts/orders.js
@@ -1,10 +1,8 @@
 const buttonOrder = document.querySelectorAll('[data-open-order]')
 const orders = document.querySelectorAll('[data-order]')
 
-
 buttonOrder.forEach((button, index) => {
   button.addEventListener('click', e => {
     orders[index].classList.toggle('open')
-    console.log(orders[index].dataset.order)
   })
 })
